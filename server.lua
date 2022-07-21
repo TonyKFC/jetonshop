425c02af6c9f0268fc279fe26bdbb69630c8e40d
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-07-21 10:43:51 -0700

    feat: Added input dialog

--- server.lua
+++ server.lua
@@ -1,89 +1,22 @@
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
- 
+
 RegisterServerEvent('jeton:add')
-AddEventHandler('jeton:add', function()
-	local money = 1000
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	 
-	if  xPlayer.getAccount('money').money >= money then
-		xPlayer.removeAccountMoney('money', money)
-		xPlayer.addInventoryItem('jeton', 10)
+AddEventHandler('jeton:add', function(amount)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local xMoney = xPlayer.getAccount('money').money
+	local price = math.floor(amount * 100)
+	if xMoney >= price then
+		xPlayer.removeAccountMoney('money', price)
+		xPlayer.addInventoryItem(jeton, amount)
 		TriggerEvent('esx_addonaccount:getSharedAccount', 'society_casino', function(account)  
 			account.addMoney(money)
 		end)
-
-	else	
-		 
+	else
 		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'you dont have enough money!', length = 2500})
-
 	end
- 	
 end)
- 
-
-
-RegisterServerEvent('jeton:addd')
-AddEventHandler('jeton:addd', function()
-	local money = 10000
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	 
-		if  xPlayer.getAccount('money').money >= money then
-			xPlayer.removeAccountMoney('money', money)
-			xPlayer.addInventoryItem('jeton', 100)
-			TriggerEvent('esx_addonaccount:getSharedAccount', 'society_casino', function(account)  
-			account.addMoney(money)
-			end)
-		else	
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'you dont have enough money!', length = 2500})
-		end
- 
-end)
-
-RegisterServerEvent('jeton:adddd')
-AddEventHandler('jeton:adddd', function()
-	local money = 100000
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
- 
-		if  xPlayer.getAccount('money').money >= money then
-			xPlayer.removeAccountMoney('money', money)
-			xPlayer.addInventoryItem('jeton', 1000)
- 
-			TriggerEvent('esx_addonaccount:getSharedAccount', 'society_casino', function(account)  
-				account.addMoney(money)
-		 
-			end)
- 
-		else	
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'you dont have enough money!', length = 2500})
-		end
-	 
-end)
-
-
-RegisterServerEvent('jeton:addddd')
-AddEventHandler('jeton:addddd', function()
-	local money = 1000000
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
- 
-		if  xPlayer.getAccount('money').money >= money then
-			xPlayer.removeAccountMoney('money', money)
-			xPlayer.addInventoryItem('jeton', 10000)
- 
-		TriggerEvent('esx_addonaccount:getSharedAccount', 'society_casino', function(account)  
-			account.addMoney(money)	 
-		end)
-		else	
-			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'you dont have enough money!', length = 2500})
-		end
-	 
-end)
-
   
 RegisterServerEvent('jeton:sellall')
 AddEventHandler('jeton:sellall', function()
