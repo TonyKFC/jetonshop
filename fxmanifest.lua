0c4c87289701120dcdc3f96d818f3e7718bf8b73
Tompizza <108546980+Tompizza@users.noreply.github.com>
2022-07-21 19:39:24 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+lua54        'yes'
+game 'gta5'
+
+author 'jeton'
+
+description 'jetonshop'
+ 
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+shared_scripts {
+	'@es_extended/imports.lua', 
+	'@ox_lib/init.lua'
+}
